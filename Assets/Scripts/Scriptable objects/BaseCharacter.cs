e99060d6176b9d355c3992963c65a5bfe9d4ec64
Frederik Ingolv Friquin <frederik.friquin@gmail.com>
2025-11-27 14:06:40 +0100

    Changed BaseCharacter script to be a scriptable object
    To improve the user-friendliness of the system I altered the BaseCharacter to be a scriptable object instead of a MonoBehavior. This script can also be used to make variant characters with extra or limited functionality

--- Assets/Scripts/Scriptable objects/BaseCharacter.cs
+++ Assets/Scripts/Scriptable objects/BaseCharacter.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
 // template script of sorts for all characters to inherit from
-public abstract class ICharacter : MonoBehaviour
+[CreateAssetMenu(menuName = "Scriptable objects/BaseCharacter")]
+public class BaseCharacter : ScriptableObject
 {
     // defining all character attributes and making accessors for the relevant ones
-    private int _speed { get; set; }
+    [SerializeField]private int _speed;
     public int Speed {get{return _speed;}}
-    private int _hitPoints { get; set; }
+    private int _hitPoints;
     public int HitPoints {get{return _hitPoints;}}
-    private int _maxHP {get; set;}
-    private int _defense { get; set; }
-    private int _mind {get; set;}
+    [SerializeField]private int _maxHP;
+    [SerializeField]private int _defense;
+    [SerializeField]private int _mind;
     private int _maxSanity {get; set;}
     private int _insanity {get; set;}
     private int _sanity {get; set;}
-    private int _accuracy {get; set;}
+    [SerializeField]private int _accuracy;
 
 // it is standard to make a separate method for changing the value of a private variable
     public int Damage(int hit)
